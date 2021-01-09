7cd8e9c4ac56f979cd988e51c1d377d90ec00c8d
sanjeev-bq <sanjeev.kumar@bloombergquint.com>
2021-01-10 01:21:17 +0530

    branching strategy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi Dcoker Cloud Setup</p>
+        <p>Hi Dcoker Cloud Setup Complete</p>
       </header>
     </div>
   );
