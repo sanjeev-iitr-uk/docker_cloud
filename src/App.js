e31397de1b60281130983ea6600555922409c347
sanjeev-bq <sanjeev.kumar@bloombergquint.com>
2021-01-09 18:40:18 +0530

    some code change

--- src/App.js
+++ src/App.js
@@ -7,15 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi Dcoker Cloud</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hi Dcoker Cloud Setup</p>
       </header>
     </div>
   );
